259881be3dfd5ce3aa740c6bb3a6da79d9af2548
onuriltan <onur.iltan@gmail.com>
2022-05-11 09:22:52 +0300

    refactor: success on discord

--- src/components/connect-discord/index.jsx
+++ src/components/connect-discord/index.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import toast from "react-hot-toast";
 import classNames from "classnames";
 import ReCAPTCHA from "react-google-recaptcha";
 
 import Logo from "../../assets/discord.svg";
 import { authenticateDiscord } from "../../services";
-import { ACTIONS } from "../../store/actions";
 import initUser from "../utils/initUser";
 import setLoading from "../utils/loading";
 
 const ConnectDiscord = () => {
   const discordName = useSelector((state) => state.discordName);
-  const dispatch = useDispatch();
   const [success, setSuccess] = useState(false);
   const recaptchaRef = useRef(null);
 
@@ -30,6 +28,26 @@ const ConnectDiscord = () => {
     window.history.pushState({}, document.title, url);
   };
 
+  const showSuccess = () => {
+    setLoading(false)
+    clearUrlParams();
+    setSuccess(true);
+    toast.success(`Discord successfully connected, ${discordName}`);
+    const canvas = document.getElementById("confetti");
+    const myConfetti = window.confetti.create(canvas, {
+      resize: true,
+      useWorker: true,
+    });
+    myConfetti({
+      particleCount: 300,
+      spread: 400,
+    });
+    setTimeout(() => {
+      window.confetti.reset();
+      setSuccess(false);
+    }, 1500);
+  }
+
   useEffect(() => {
     const authenticate = async () => {
       const urlParams = new URLSearchParams(window.location.search);
@@ -39,39 +57,11 @@ const ConnectDiscord = () => {
           setLoading(true)
           const captchaToken = await recaptchaRef.current.executeAsync();
           recaptchaRef.current.reset();
-          const discordName = await authenticateDiscord({code, captchaToken});
+          await authenticateDiscord({code, captchaToken});
           await initUser();
-          dispatch({
-            type: ACTIONS.SET_DISCORD_NAME,
-            payload: {
-              data: discordName,
-            },
-          });
-          setLoading(false)
-          clearUrlParams();
-          setSuccess(true);
-          toast.success(`Discord successfully connected, ${discordName}`);
-          const canvas = document.getElementById("confetti");
-          const myConfetti = window.confetti.create(canvas, {
-            resize: true,
-            useWorker: true,
-          });
-          myConfetti({
-            particleCount: 300,
-            spread: 400,
-          });
-          setTimeout(() => {
-            window.confetti.reset();
-            setSuccess(false);
-          }, 1500);
+         showSuccess()
         } catch (e) {
           setLoading(false)
-          dispatch({
-            type: ACTIONS.SET_DISCORD_NAME,
-            payload: {
-              data: "",
-            },
-          });
           toast.error(e.message);
           clearUrlParams();
         }
