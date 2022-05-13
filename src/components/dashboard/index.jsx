5ede68e70b7e3c3ddeca3b8b323171bbb4efb3cf
onuriltan <onur.iltan@gmail.com>
2022-05-13 15:39:49 +0300

    feat: check mint date, then show nft stats

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
 import { useSelector } from "react-redux";
@@ -8,6 +8,18 @@ const Dashboard = () => {
   const isDiscordMember = useSelector((state) => state.isDiscordMember);
   const ownedNFTCount = useSelector((state) => state.ownedNFTCount);
   const hasRare = useSelector((state) => state.hasRare);
+
+  const [isPassedMintDate, setIsPassedMintDate] = useState(false);
+
+  const checkMintDate = () => {
+    const mintDate = new Date(process.env.REACT_APP_MINT_DATE);
+    const today = new Date();
+    setIsPassedMintDate(mintDate < today);
+  };
+
+  useEffect(() => {
+    checkMintDate();
+  }, []);
 
   return (
     <div className="dashboard">
@@ -43,43 +55,45 @@ const Dashboard = () => {
             </div>
           )}
         </div>
-         {ownedNFTCount === 0 && (
+
+        {ownedNFTCount === 0 && isPassedMintDate && (
           <div className="dashboard__body__item">
-            <i className="fa-solid fa-xmark fail_color"></i> 
-            {" "}You should have at least 1 {process.env.REACT_APP_PROJECT_NAME} that is <b>not listed for sale</b>
+            <i className="fa-solid fa-xmark fail_color"></i> You should have at
+            least 1 {process.env.REACT_APP_PROJECT_NAME} that is{" "}
+            <b>not listed for sale</b>
           </div>
         )}
-        {ownedNFTCount === 0 && (
+        {ownedNFTCount === 0 && isPassedMintDate && (
           <div className="dashboard__body__item">
             <i className="fa-solid fa-xmark fail_color"></i> You need at least 1
             Maniax NFT to have <b>Maniac Role</b> in Discord Server
           </div>
         )}
-        {ownedNFTCount < 5 && (
+        {ownedNFTCount < 5 && isPassedMintDate && (
           <div className="dashboard__body__item">
             <i className="fa-solid fa-xmark fail_color"></i> You need at least 5
             Maniax NFTs to have <b>ManiaX Role</b> in Discord Server
           </div>
         )}
-        {ownedNFTCount > 0 && (
+        {ownedNFTCount > 0 && isPassedMintDate && (
           <div className="dashboard__body__item">
             <i className="fa-solid fa-check success_color"></i> You have{" "}
             <b>Maniac Role</b> in our Discord Server!
           </div>
         )}
-        {ownedNFTCount >= 5 && (
+        {ownedNFTCount >= 5 && isPassedMintDate && (
           <div className="dashboard__body__item">
             <i className="fa-solid fa-check success_color"></i> You have{" "}
             <b>Maniax Role</b> in our Discord Server!
           </div>
         )}
-        {!hasRare && ownedNFTCount >= 0  && (
+        {!hasRare && ownedNFTCount >= 0 && isPassedMintDate && (
           <div className="dashboard__body__item">
-            <i className="fa-solid fa-xmark fail_color"></i>{" "}You need at
-            least 1 Rare Maniax NFT to get <b>RareX Role</b> in Discord Server
+            <i className="fa-solid fa-xmark fail_color"></i> You need at least 1
+            Rare Maniax NFT to get <b>RareX Role</b> in Discord Server
           </div>
         )}
-        {hasRare && ownedNFTCount > 0 && (
+        {hasRare && ownedNFTCount > 0 && isPassedMintDate && (
           <div>
             <i className="fa-solid fa-check success_color"></i> You have{" "}
             <b>RareX Role</b> in our Discord Server!
